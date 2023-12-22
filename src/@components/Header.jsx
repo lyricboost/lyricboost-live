759b3e3f50b29eff5b2b4e27625a2f260c8da098
lyricboost <info@lyricboost.com>
2023-12-22 13:30:30 -0600

    Restructured Header component

--- src/@components/Header.jsx
+++ src/@components/Header.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Grid, Container } from '@mui/material';
-
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import LyricBoostLogo from '/LyricBoostLogo.svg'
-
-
+import { AppBar, Toolbar, Container } from '@mui/material';
+ 
 const Header = () => {
-
     return (
         <div>
             <AppBar position="static" style={{ backgroundColor: "#2c3e50" }}>
                 <Container maxWidth="xl">
                     <Toolbar style={{ display: "flex", alignItems: "center", padding: "10px 0px" }}> 
-                            <img src={LyricBoostLogo} height="40px"/>
+                        {/* Update the src to point to the public directory */}
+                        <img src="/LyricBoostLogo.svg" height="40px" alt="LyricBoost Logo"/>
                     </Toolbar>
                 </Container>
             </AppBar>
