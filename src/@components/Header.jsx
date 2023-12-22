45d3c4437b846b49d1913a0f012d04597aec2998
lyricboost <info@lyricboost.com>
2023-12-22 13:49:28 -0600

    Updated for logo retrieval

--- src/@components/Header.jsx
+++ src/@components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Container } from '@mui/material';
+import LyricBoostLogo from './../assets/LyricBoostLogo.svg';
+
  
 const Header = () => {
     return (
@@ -8,7 +10,7 @@ const Header = () => {
                 <Container maxWidth="xl">
                     <Toolbar style={{ display: "flex", alignItems: "center", padding: "10px 0px" }}> 
                         {/* Update the src to point to the public directory */}
-                        <img src="/LyricBoostLogo.svg" height="40px" alt="LyricBoost Logo"/>
+                        <img src={LyricBoostLogo} height="40px" alt="LyricBoost Logo"/>
                     </Toolbar>
                 </Container>
             </AppBar>
