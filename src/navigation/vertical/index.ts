3ef41981e347d79ca4cb84099754b8392f9ea3e7
lyricboost <info@lyricboost.com>
2023-12-23 01:08:28 -0600

    Updated nav links

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -4,21 +4,14 @@ import { VerticalNavItemsType } from 'src/@core/layouts/types'
 const navigation = (): VerticalNavItemsType => {
   return [
     {
-      title: 'Home',
-      path: '/home',
-      icon: 'mdi:home-outline',
+      title: 'Dashboard',
+      path: '/dashboard',
+      icon: 'mdi:view-dashboard'
     },
     {
-      title: 'Second Page',
-      path: '/second-page',
-      icon: 'mdi:email-outline',
-    },
-    {
-      path: '/acl',
-      action: 'read',
-      subject: 'acl-page',
-      title: 'Access Control',
-      icon: 'mdi:shield-outline',
+      title: 'Song Book',
+      path: '/song-book',
+      icon: 'mdi:book'
     }
   ]
 }
